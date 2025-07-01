321dc34c1e455271dfd62c97731c22230d076c52
Pswie <langellotti19@live.it>
2025-07-01 04:57:42 +0200

    111

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'H24 Langellotti ',
+  description: 'Distributori automatici attivi 24 ore su 24, 7 giorni su 7',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
